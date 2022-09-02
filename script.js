6fd3c2c143b6c74ff57601a6751e70db9138a38e
RoseThorn10 <lrroman@comcast.net>
2022-09-01 22:45:13 -0500

    Edit script,js

--- script.js
+++ script.js
@@ -13,44 +13,61 @@ generateBtn.addEventListener("click", writePassword);
 // This was done with the help of the great TA, Paul!
 function generatePassword() {
   var availableCharacters = '';
-  var completedPassword = '';
-  // Ask for password length
-  var pwdLength = 0
-  while (pwdLength < 8 || pwdLength > 128) {
-    pwdLength = parseInt(prompt(`How long do you want the password? Must be 8-128 characters`));
+
+  // Loop until at least one character type is selected
+  while (availableCharacters.length === 0) {
+
+    var completedPassword = '';
+    // Ask for password length
+    var pwdLength = 0;
+    while (pwdLength < 8 || pwdLength > 128) {
+      var length = prompt(`How long do you want the password? Must be 8-128 characters`);
+      console.log(length);
+      if (length == null) {
+        pwdLength = 0;
+      } else if (parseInt(length) > 0) {
+        pwdLength = parseInt(length);
+      }
+
+      console.log(pwdLength);
+      if (pwdLength < 8 || pwdLength > 128) { // input must be 8-128
+        alert('Password must be between 8 and 128 characters')
+      };
+
+    }
     
-    if (pwdLength < 8 || pwdLength > 128) { // input must be 8-128
-       alert('Password must be between 8 and 128 characters')
-   };
+    // Does user want uppercase letters?
+    var upperLtrs = confirm('Do you want to use uppercase letters?');
+    // If yes, adds uppercase letters to string
+    if (upperLtrs) {
+      availableCharacters = availableCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    }
+    console.log(upperLtrs);
 
-  }
-  
-  // Does user want uppercase letters?
-  var upperLtrs = confirm('Do you want to use uppercase letters?');
-  // If yes, adds uppercase letters to string
-  if (upperLtrs) {
-    availableCharacters = availableCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  }
-  console.log(upperLtrs);
+    // Does user want lowercase letters?
+    var lowerLtrs = confirm('Do you want to use lowercase letters?');
+    // If yes, adds lowercase letters to string
+    if (lowerLtrs) {
+      availableCharacters = availableCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase();
+    }
+    // Does user want special characters?
+    var specChar = confirm('Do you want to use special characters?');
+    // If yes, adds special characters to string
+    if (specChar) {
+      availableCharacters = availableCharacters + '!@#$%^&*()_-+=[]{}';
+    }
+    // Does user want numbers?
+    var nums = confirm('Do you want to use numbers?');
+    // If yes, adds numbers to string
+    if (nums) {
+      availableCharacters = availableCharacters + '1234567890';
+    }
 
-  // Does user want lowercase letters?
-  var lowerLtrs = confirm('Do you want to use lowercase letters?');
-  // If yes, adds lowercase letters to string
-  if (lowerLtrs) {
-    availableCharacters = availableCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase();
+    if (availableCharacters.length === 0) {
+      alert('Please select at least one character type')
+    }
   }
-  // Does user want special characters?
-  var specChar = confirm('Do you want to use special characters?');
-  // If yes, adds special characters to string
-  if (specChar) {
-    availableCharacters = availableCharacters + '!@#$%^&*()_-+=[]{}';
-  }
-  // Does user want numbers?
-  var nums = confirm('Do you want to use numbers?');
-  // If yes, adds numbers to string
-  if (nums) {
-    availableCharacters = availableCharacters + '1234567890';
-  }
+
   
   for(i = 0; i < pwdLength; i++) {
     // get a random index based on the length of our character bank and use that to select one character from that bank and then concatenate it onto the password
@@ -62,5 +79,13 @@ function generatePassword() {
 
   
 }
+
+function validatePwd(password, upperLtrs, lowerLtrs, specChar, nums) {
+
+
+
+
+  return true;
+} 
 
 
